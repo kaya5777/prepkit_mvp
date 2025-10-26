9103ad7df32c8bc8e816fee4fd5bb29ad8272018
Manabu Kayamoto <manabukayamoto@ManabunoMacBook-Air-2.local>
2025-10-26 15:10:57 +0900

    hosts追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Allow Render.com hostname
+  config.hosts << "prepkit-mvp.onrender.com"
 end
