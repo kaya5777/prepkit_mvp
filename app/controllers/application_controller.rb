a481798cbc302ee1164f110ae89f671361846d04
Manabu Kayamoto <manabukayamoto@ManabunoMacBook-Air-2.local>
2025-11-15 20:01:46 +0900

    同上

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :devise_controller?
   before_action :set_sidebar_histories
 
   rescue_from ActionController::ParameterMissing, with: :render_bad_request
